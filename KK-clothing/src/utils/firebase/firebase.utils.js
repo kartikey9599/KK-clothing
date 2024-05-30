e27cd817c1b8edf11dcd02cec675ae9d06991574
Kartikey <kartikeygupta9599@gmail.com>
2024-05-30 14:27:08 +0530

    connected firebase db with frontend and now we can add users in db.

--- KK-clothing/src/utils/firebase/firebase.utils.js
+++ KK-clothing/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCTC10aY12iU4H93fuKhHgZTKx3dKoRD3o",
   authDomain: "kk-clothing-db-2944c.firebaseapp.com",
@@ -24,3 +26,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+  console.log(userSnapShot);
+  console.log(userSnapShot.exists());
+
+  if (!userSnapShot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("error creating the user ", error.message);
+    }
+  }
+  return userDocRef;
+};
