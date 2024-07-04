81e89870510bdc3f643f3eecb710341b6bad7df3
Kartikey <kartikeygupta9599@gmail.com>
2024-07-04 17:09:24 +0530

    done with checkout page

--- KK-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ KK-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../CONTEXTS/Cart.context";
 import "./cart-dropdown.styles.scss";
 import Button from "../button/Button.component";
@@ -6,6 +7,10 @@ import CartItem from "../cart-items/cart-item.component";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -13,7 +18,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
