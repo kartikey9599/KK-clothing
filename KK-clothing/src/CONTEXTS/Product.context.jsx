ea0da4610b5b0f33d4c9fc62026ea70359e9f4e6
Kartikey <kartikeygupta9599@gmail.com>
2024-07-11 14:45:43 +0530

    ok

--- KK-clothing/src/CONTEXTS/Product.context.jsx
+++ KK-clothing/src/CONTEXTS/Product.context.jsx
@@ -1,13 +1,14 @@
-import { createContext, useState } from "react";
-
-import PRODUCTS from "../shop-data.json";
+import { createContext, useEffect, useState } from "react";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
   Products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
+
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
--- KK-clothing/src/utils/firebase/firebase.utils.js
+++ KK-clothing/src/utils/firebase/firebase.utils.js
@@ -10,7 +10,14 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCTC10aY12iU4H93fuKhHgZTKx3dKoRD3o",
@@ -35,6 +42,23 @@ export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd,
+  feild = "title"
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object[feild].toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("done");
+};
 
 export const createUserDocumentFromAuth = async (
   userAuth,
