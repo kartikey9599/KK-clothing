c0e4fcb9ba4409f91ca0d4e7d04c453c6d8a07e3
Kartikey <kartikeygupta9599@gmail.com>
2024-07-17 16:49:31 +0530

    Updated shop component

--- KK-clothing/src/main.jsx
+++ KK-clothing/src/main.jsx
@@ -4,18 +4,18 @@ import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./CONTEXTS/User.context.jsx";
-import { ProductsProvider } from "./CONTEXTS/Product.context.jsx";
+import { CategoriesProvider } from "./CONTEXTS/Categories.context.jsx";
 import { CartProvider } from "./CONTEXTS/Cart.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
